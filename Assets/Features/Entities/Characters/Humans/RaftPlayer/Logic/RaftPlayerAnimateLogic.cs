b7b08bdf0fc9beda687614443b5570bb65e66e51
Aidan Smith <83699315+shinysteel@users.noreply.github.com>
2026-04-11 12:06:10 +1000

    Local held items

--- Assets/Features/Entities/Characters/Humans/RaftPlayer/Logic/RaftPlayerAnimateLogic.cs
+++ Assets/Features/Entities/Characters/Humans/RaftPlayer/Logic/RaftPlayerAnimateLogic.cs
@@ -8,6 +8,7 @@ namespace FishFlingers.Entities
         private CharacterModel _model;
 
         private const string IsMovingName = "IsMoving";
+        private const string IsHoldingItemName = "IsHoldingItem";
 
         public RaftPlayerAnimateLogic(RaftPlayer player, CharacterModel model)
         {
@@ -18,8 +19,10 @@ namespace FishFlingers.Entities
         public void Tick()
         {
             bool isMoving = _player.InputLogic.MoveDirection != Vector3.zero;
-
+            bool isHoldingItem = _player.Hotbar.SelectedItem != null;
+            
             _model.Animator.SetBool(IsMovingName, isMoving);
+            _model.Animator.SetBool(IsHoldingItemName, isHoldingItem);
         }
     }
 }
