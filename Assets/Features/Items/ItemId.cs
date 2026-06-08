8f65c48d27fb83aeb8d6c979f4963cd967be1d32
Aidan Smith <83699315+shinysteel@users.noreply.github.com>
2026-06-08 19:20:45 +1000

    Added ToothSpear and MakoTooth

--- Assets/Features/Items/ItemId.cs
+++ Assets/Features/Items/ItemId.cs
@@ -4,14 +4,16 @@ namespace FishFlingers.Items
 {
     public enum ItemId
     {
-        None      = 0,
-        Driftwood = 1,
-        Paddle    = 2,
-        Hammer    = 3,
-        Scrap     = 4,
-        Goop      = 5,
-        PalmSeed  = 6,
-        SharkFin  = 7,
-        FinBack   = 8,
+        None       = 0,
+        Driftwood  = 1,
+        Paddle     = 2,
+        Hammer     = 3,
+        Scrap      = 4,
+        Goop       = 5,
+        PalmSeed   = 6,
+        SharkFin   = 7,
+        FinBack    = 8,
+        ToothSpear = 9,
+        MakoTooth  = 10,
     }
 }
